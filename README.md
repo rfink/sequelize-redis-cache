d4e5e53878a4d0e32eed815444728f569eec1532
Ryan Fink <ryanjfink@gmail.com>
2014-09-19 19:36:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+sequelize-redis-cache
+=====================
+
+Small fluent interface for caching sequelize database query results in redis more easily.
+Simply put, this is a wrapper around sequelize retrieval methods that will automatically
+check in the configured redis instance for a value (based on a hash of the query and
+model name), then retrieve from the database and persist in redis if not found.  It is
+promise based, so it will resemble sequelize for the most part, and be co/koa friendly.
+
+Installation
+=====================
+
+```
+
+npm install sequelize-redis-cache
+
+```
+
+Usage
+=====================
+
+```javascript
+
+var initCacher = require('sequelize-redis-cache');
+var redis = require('redis');
+var Sequelize = require('sequelize');
+
+var rc = redis.createClient(6379, 'localhost');
+var db = new Sequelize('cache_tester', 'root', 'root', { dialect: 'mysql' });
+var cacher = initCache(db, rc);
+
+var cacheObj = cacher('sequelize-model-name')
+  .ttl(5);
+cacheObj.find({ where: { id: 3 } })
+  .then(function(row) {
+    console.log(row); // sequelize db object
+    console.log(cacheObj.cacheHit); // true or false
+  });
+
+```
+
+Check the tests out for more info, but it's pretty simple.  The currently supported
+methods are:
+
+  find
+  findAll
+  findAndCountAll
+  all
+  min
+  max
+  sum
+
+License
+====================
+MIT - Rekt
---
+++ test/sequelize-redis-cache.test.js
@@ -0,0 +1,105 @@
+
+var redis = require('redis');
+var Sequelize = require('sequelize');
+var should = require('should');
+var initCache = require('..');
+
+var opts = {};
+opts.database = process.env.DB_NAME || 'sequelize_redis_cache_test';
+opts.user = process.env.DB_USER || 'root';
+opts.password = process.env.DB_PASS;
+opts.dialect = process.env.DB_DIALECT || 'sqlite';
+opts.logging = process.env.DB_LOG ? console.log : false;
+
+var redisPort = process.env.REDIS_PORT || 6379;
+var redisHost = process.env.REDIS_HOST;
+
+/*global describe*/
+/*global it*/
+/*global before*/
+/*global after*/
+
+function onErr(err) {
+  throw err;
+}
+
+describe('Sequelize-Redis-Cache', function() {
+  var rc;
+  var db;
+  var Entity;
+  var inst;
+  var cacher;
+
+  before(function(done) {
+    rc = redis.createClient(redisPort, redisHost);
+    rc.on('error', onErr);
+    db = new Sequelize(opts.database, opts.user, opts.password, opts);
+    cacher = initCache(db, rc);
+    Entity = db.define('entity', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: Sequelize.STRING(255)
+    });
+    Entity.sync({ force: true })
+      .success(function() {
+        Entity.create({ name: 'Test Instance' }).success(function(entity) {
+          inst = entity;
+          return done();
+        })
+        .error(onErr);
+      })
+      .error(onErr);
+  });
+
+  it('should fetch stuff from database with and without cache', function(done) {
+    var query = { where: { createdAt: inst.createdAt } };
+    var obj = cacher('entity')
+      .ttl(1);
+    return obj.find(query)
+      .then(function(res) {
+        obj.cacheHit.should.equal(false);
+        var obj2 = cacher('entity')
+          .ttl(1);
+        return obj2.find(query)
+          .then(function(res) {
+            should.exist(res);
+            obj2.cacheHit.should.equal(true);
+            obj2.clearCache().then(function() {
+              return done();
+            }, onErr);
+          }, onErr);
+      }, onErr);
+  });
+
+  it('should not hit cache if no results', function(done) {
+    var obj = cacher('entity')
+      .ttl(1);
+    return obj.find({ where: { id: 2 } })
+      .then(function(res) {
+        should.not.exist(res);
+        obj.cacheHit.should.equal(false);
+        return done();
+      }, onErr);
+  });
+
+  it('should clear the cache correctly', function(done) {
+    var query = { where: { createdAt: inst.createdAt } };
+    var obj = cacher('entity')
+      .ttl(1);
+    return obj.find(query)
+      .then(function(res) {
+        var key = obj.key();
+        obj.clearCache(query)
+          .then(function() {
+            rc.get(key, function(err, res) {
+              should.not.exist(err);
+              should.not.exist(res);
+              return done();
+            });
+          }, onErr);
+      }, onErr);
+  });
+});
